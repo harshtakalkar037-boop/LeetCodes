7aa89b5d533dcbc341f0e737bb50f94fe93b13fa
Harsh Takalkar <harshtakalkar037@gmail.com>
2026-07-29 20:43:34 +0530

    Time: 0 ms (100.00%) | Memory: 44.1 MB (16.59%) - LeetSync

---
+++ 48-rotate-image/rotate-image.java
@@ -0,0 +1,22 @@
+class Solution {
+
+    public void rotate(int[][] matrix) {
+
+        int n = matrix.length;
+        int[][] ans = new int[n][n];
+
+        // Build rotated matrix
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < n; j++) {
+                ans[j][n - 1 - i] = matrix[i][j];
+            }
+        }
+
+        // Copy back
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < n; j++) {
+                matrix[i][j] = ans[i][j];
+            }
+        }
+    }
+}
