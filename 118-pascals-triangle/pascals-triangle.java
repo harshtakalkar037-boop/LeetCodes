319bf9c5c3a8c3514f147061c8708fa95617e295
Harsh Takalkar <harshtakalkar037@gmail.com>
2026-08-03 19:02:03 +0530

    Time: 0 ms (100.00%) | Memory: 43.6 MB (34.81%) - LeetSync

--- 118-pascals-triangle/pascals-triangle.java
+++ 118-pascals-triangle/pascals-triangle.java
@@ -1,5 +1,6 @@
 class Solution {
     public List<List<Integer>> generate(int numRows) {
+        /* # Striver using formula 
         // Result list to hold all rows
         List<List<Integer>> triangle = new ArrayList<>();
 
@@ -18,6 +19,33 @@ class Solution {
             // Add current row to the triangle
             triangle.add(row);
         }
-        return triangle;
+        return triangle; */
+
+        //Using DP 
+        List<List<Integer>> result = new ArrayList<>();
+        if (numRows == 0) {
+            return result;
+        }
+
+        if (numRows == 1) {
+            List<Integer> firstRow = new ArrayList<>();
+            firstRow.add(1);
+            result.add(firstRow);
+            return result;
+        }
+
+        result = generate(numRows - 1);
+        List<Integer> prevRow = result.get(numRows - 2);
+        List<Integer> currentRow = new ArrayList<>();
+        currentRow.add(1);
+
+        for (int i = 1; i < numRows - 1; i++) {
+            currentRow.add(prevRow.get(i - 1) + prevRow.get(i));
+        }
+
+        currentRow.add(1);
+        result.add(currentRow);
+
+        return result;
     }
 }
