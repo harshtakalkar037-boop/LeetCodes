5999d643e3d353a6a6c15a97846bdbdcd8975020
Harsh Takalkar <harshtakalkar037@gmail.com>
2026-08-05 00:19:42 +0530

    Time: 21 ms (41.75%) | Memory: 45.6 MB (89.09%) - LeetSync

--- 18-4sum/4sum.java
+++ 18-4sum/4sum.java
@@ -1,32 +1,56 @@
 class Solution {
+
     public List<List<Integer>> fourSum(int[] nums, int target) {
 
         Arrays.sort(nums);
 
-        Set<List<Integer>> ans = new HashSet<>();
+        List<List<Integer>> ans = new ArrayList<>();
 
         int n = nums.length;
 
-        for(int i=0;i<n;i++){
+        for(int i=0;i<n-3;i++){
 
-            for(int j=i+1;j<n;j++){
+            if(i>0 && nums[i]==nums[i-1]) continue;
 
-                HashSet<Long> set = new HashSet<>();
+            for(int j=i+1;j<n-2;j++){
 
-                for(int k=j+1;k<n;k++){
+                if(j>i+1 && nums[j]==nums[j-1]) continue;
 
-                    long fourth = (long)target - nums[i] - nums[j] - nums[k];
+                int left=j+1;
+                int right=n-1;
 
-                    if(set.contains(fourth)){
+                while(left<right){
 
-                        ans.add(Arrays.asList(nums[i], nums[j], (int)fourth, nums[k]));
+                    long sum=(long)nums[i]+nums[j]+nums[left]+nums[right];
+
+                    if(sum==target){
+
+                        ans.add(Arrays.asList(nums[i],nums[j],nums[left],nums[right]));
+
+                        left++;
+                        right--;
+
+                        while(left<right && nums[left]==nums[left-1]) left++;
+
+                        while(left<right && nums[right]==nums[right+1]) right--;
+
                     }
 
-                    set.add((long)nums[k]);
+                    else if(sum<target){
+
+                        left++;
+
+                    }
+
+                    else{
+
+                        right--;
+
+                    }
                 }
             }
         }
 
-        return new ArrayList<>(ans);
+        return ans;
     }
 }
