195ed905d3d2843c28aafd74686c9bbf1f985c6e
Harsh Takalkar <harshtakalkar037@gmail.com>
2026-08-10 08:16:15 +0530

    Time: 14 ms (91.38%) | Memory: 42.4 MB (83.55%) - LeetSync

--- 1617-stone-game-iv/stone-game-iv.java
+++ 1617-stone-game-iv/stone-game-iv.java
@@ -1,4 +1,4 @@
-class Solution {
+/*class Solution {
     static final int MAX = 100000;
     static final boolean[] dp = new boolean[MAX + 1];
 
@@ -12,4 +12,21 @@ class Solution {
     }
 
     public boolean winnerSquareGame(int n) { return dp[n]; }
+}*/
+
+class Solution {
+    public boolean winnerSquareGame(int n) {
+        boolean[] dp = new boolean[n + 1];
+
+        for (int i = 1; i <= n; i++) {
+            for (int j = 1; j * j <= i; j++) {
+                if (!dp[i - j * j]) {
+                    dp[i] = true;
+                    break;
+                }
+            }
+        }
+
+        return dp[n];
+    }
 }
