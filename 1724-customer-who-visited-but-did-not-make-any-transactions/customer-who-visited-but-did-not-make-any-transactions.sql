43287c6c7b191bc637f7bb802aa48814f83c898d
Harsh Takalkar <harshtakalkar037@gmail.com>
2026-08-22 22:18:24 +0530

    Time: 1414 ms (68.20%) | Memory: 0B (100.00%) - LeetSync

--- 1724-customer-who-visited-but-did-not-make-any-transactions/customer-who-visited-but-did-not-make-any-transactions.sql
+++ 1724-customer-who-visited-but-did-not-make-any-transactions/customer-who-visited-but-did-not-make-any-transactions.sql
@@ -1,6 +1,12 @@
-# Write your MySQL query statement below
-SELECT customer_id, COUNT(v.visit_id) as count_no_trans 
+SELECT
+    customer_id,
+    COUNT(*) AS count_no_trans
+
 FROM Visits v
-LEFT JOIN Transactions t ON v.visit_id = t.visit_id
+
+LEFT JOIN Transactions t
+    ON v.visit_id = t.visit_id
+
 WHERE transaction_id IS NULL
-GROUP BY customer_id
+
+GROUP BY customer_id;
