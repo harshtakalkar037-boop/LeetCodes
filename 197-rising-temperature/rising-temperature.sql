fe2cf6943b03f7fb0b5f255e02be178e56829932
Harsh Takalkar <harshtakalkar037@gmail.com>
2026-08-24 19:20:25 +0530

    Time: 582 ms (25.32%) | Memory: 0B (100.00%) - LeetSync

--- 197-rising-temperature/rising-temperature.sql
+++ 197-rising-temperature/rising-temperature.sql
@@ -1,11 +1,3 @@
-# Write your MySQL query statement below
-#select today.id
-#from weather yesterday 
-#cross join weather today 
-
-#where datediff(today.recordDate,yesterday.recordDate)=1
-    #and today.temperature >yesterday.temperature
-
-    # optimised 
-    select w2.id from Weather w1 join Weather w2 on w1.recordDate=date_sub(w2.recordDate,interval 1 day) 
-where w2.temperature>w1.temperature;
+SELECT w1.id
+FROM Weather w1, Weather w2
+WHERE DATEDIFF(w1.recordDate, w2.recordDate) = 1 AND w1.temperature > w2.temperature;
