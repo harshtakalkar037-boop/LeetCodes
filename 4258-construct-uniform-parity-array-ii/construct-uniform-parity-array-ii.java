128e93018762882eb9c2fa7c19d363920a702ae7
Harsh Takalkar <harshtakalkar037@gmail.com>
2026-09-03 08:16:53 +0530

    Time: 5 ms (83.33%) | Memory: 121.4 MB (81.16%) - LeetSync

--- 4258-construct-uniform-parity-array-ii/construct-uniform-parity-array-ii.java
+++ 4258-construct-uniform-parity-array-ii/construct-uniform-parity-array-ii.java
@@ -1,12 +1,12 @@
 class Solution {
-    public boolean uniformArray(int[] nums) {
-        int min = nums[0], odd = 0;
-
-        for (int x : nums) {
-            min = Math.min(min, x);
-            odd |= x & 1;
+    public boolean uniformArray(int[] a) {
+        int mn = Integer.MAX_VALUE;
+        int oddCnt = 0;
+        for (int x : a) {
+            mn = Math.min(mn, x);
+            if (x % 2 == 1) oddCnt++;
         }
-
-        return (min & 1) == odd;
+        // min Element is ODD(remaining even > min) or All Even!
+        return mn % 2 != 0 || oddCnt == 0;
     }
 }
